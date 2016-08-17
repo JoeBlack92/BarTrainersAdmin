7e17b16758f5f6ae9b0b61030b4247e37ea21526
iw-poblenou2 <jmsmov39@gmail.com>
2016-08-17 14:29:36 +0200

    Update 6

--- server/methods/curso.js
+++ server/methods/curso.js
@@ -24,27 +24,76 @@ Meteor.methods({
 
     anadirAlumno : function (cursoId, alumnoId) {
 
+        var curs = Cursos.findOne({_id: cursoId});
 
-        Cursos.update({_id: cursoId}, {$push: {alumnos: alumnoId}});
+        var al = Meteor.users.findOne({_id: {$in: curs.alumnos}});
 
-        Niveles.insert({
+        if(al){
+            console.log(al._id + " == " + alumnoId);
 
-            cursoId: cursoId,
-            alumnoId: alumnoId,
-            niveles: {
-                nivel1: false,
-                nivel2: false,
-                nivel3: false
+            if(al._id == alumnoId){
+
+                throw new Meteor.Error('exist', 'El alumno ya esta ingresado');
+
+            }else{
+
+                Cursos.update({_id: cursoId}, {$push: {alumnos: alumnoId}});
+
+                Niveles.insert({
+
+                    cursoId: cursoId,
+                    alumnoId: alumnoId,
+                    niveles: {
+                        nivel1: false,
+                        nivel2: false,
+                        nivel3: false
+                    }
+
+                });
+
             }
+        }else{
+            Cursos.update({_id: cursoId}, {$push: {alumnos: alumnoId}});
 
-        });
+            Niveles.insert({
+
+                cursoId: cursoId,
+                alumnoId: alumnoId,
+                niveles: {
+                    nivel1: false,
+                    nivel2: false,
+                    nivel3: false
+                }
+
+            });
+        }
+
+
+
 
 
     },
 
     anadirProfesor : function (cursoId, profesorId) {
 
-        Cursos.update({_id: cursoId}, {$push: {profesores: profesorId}});
+        var curs = Cursos.findOne({_id: cursoId});
+
+        var pro= Meteor.users.findOne({_id: {$in: curs.profesores}});
+
+        if(pro) {
+            console.log(pro._id + " == " + profesorId);
+
+            if (pro._id == profesorId) {
+
+                throw new Meteor.Error('exist', 'El profesor ya esta ingresado');
+
+            } else {
+
+                Cursos.update({_id: cursoId}, {$push: {profesores: profesorId}});
+            }
+        }else{
+            Cursos.update({_id: cursoId}, {$push: {profesores: profesorId}});
+        }
 
     },
 
