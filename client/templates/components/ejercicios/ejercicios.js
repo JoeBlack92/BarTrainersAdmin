9d03f9d65e6630615390ad7773fa92be10bf5d8f
iw-poblenou2 <jmsmov39@gmail.com>
2016-08-31 13:07:13 +0200

    Update 23

--- client/templates/components/ejercicios/ejercicios.js
+++ client/templates/components/ejercicios/ejercicios.js
@@ -6,6 +6,7 @@ Template.ejercicios.helpers({
 });
 
 Template.ejercicios.events({
+    
   'click #nuevoEjer' : function () {
 
       if($('#nombreEjercicio').val()){
@@ -39,50 +40,6 @@ Template.ejercicios.events({
 });
 
 Template.ejercicios.onCreated(function() {
-
-    // ANIMATION_END = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
-    //
-    // debouncer = function (fn) {
-    //     var stack = [];
-    //     var threshold = 200;
-    //     var lastCall = new Date().getTime();
-    //     var callFunctions = Array.prototype.forEach.bind(stack, function (fn) { fn(); }); // can't wait for `x => x()`
-    //     var timeoutHandle;
-    //     return function () {
-    //         var now = new Date().getTime()
-    //         var deltaT = now - lastCall;
-    //         lastCall = now;
-    //         // make sure each function is called with the correct `this` and arguments
-    //         stack.push(fn.bind.apply(fn, [this].concat(Array.prototype.slice.call(arguments))));
-    //         clearTimeout(timeoutHandle);
-    //         timeoutHandle = setTimeout(callFunctions, threshold);
-    //     };
-    // };
-    //
-    // animating = false;
-    //
-    // animateEnter = function  () {
-    //     if (animating) { return; }
-    //     animating = true;
-    //     $('.item').addClass('animate-enter');
-    //     $('.list').removeClass('hidden');
-    //     $('.item').one(ANIMATION_END, debouncer(function (e) {
-    //         $(this).removeClass('animate-enter');
-    //         animating = false;
-    //     }));
-    // }
-    //
-    // animateLeave = function () {
-    //     if (animating) { return; }
-    //     animating = true;
-    //     $('.item').addClass('animate-leave');
-    //     $('.item').one(ANIMATION_END, debouncer(function (e) {
-    //         $('.list').addClass('hidden');
-    //         $(this).removeClass('animate-leave');
-    //         animating = false;
-    //     }));
-    // }
-
 
 
 });
--- client/templates/components/evaluador/evaluar.js
+++ client/templates/components/evaluador/evaluar.js
@@ -1,21 +1,59 @@
+
+Template.registerHelper('count', function () {
+    temp++;
+    if(temp <= 1){
+        count++;
+    }else{
+        temp = 0;
+
+    }
+
+    return count;
+
+});
+
 Template.evaluar.helpers({
- 
+ listaeval: function () {
+
+     var al = Niveles.findOne({idAlumno:Router.current().params.IdAlumno});
+    
+         return al.ejercicios;
+     
+ },
+    alumno : function () {
+        var alr = Meteor.users.findOne({_id: Router.current().params.IdAlumno});
+        return alr.profile.nombre + " " + alr.profile.apellido;
+        
+    }
 });
 
 Template.evaluar.events({
- //add your events here
+  'change input' : function () {
+
+      var value = this.valueOf();
+      var val = $( "input:checked" ).val();
+      if(val == "undefined"){
+          val = false;
+      }
+      console.log(value.name + " valor actual: " + val);
+  }
 });
 
 Template.evaluar.onCreated(function() {
    var inf = Router.current().params;
     console.log(inf.IdCurso);
+    count = 0;
+    temp = 0;
 });
 
 Template.evaluar.onRendered(function() {
-    //add your statement here
+
+
+    animateEnter();
+
 });
 
 Template.evaluar.onDestroyed(function() {
-    //add your statement here
+    animateLeave();
 });
 
